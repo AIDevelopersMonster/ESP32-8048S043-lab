0119641017b21477148459f761dcb76ce60d61ff
AlexMalachevsky <t89257409519@gmail.com>
2026-08-25 13:27:56 +0300

    Add WiFi secrets template for ESP32-8048S043

---
+++ libraries/ESP32_8048S043/examples/06_WiFiTest/wifi_secrets.example.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Copy this file to wifi_secrets.h and fill it locally.
+// Do not commit wifi_secrets.h.
+
+static const char *WIFI_TEST_SSID = "YOUR_WIFI_SSID";
+static const char *WIFI_TEST_PASSWORD = "YOUR_WIFI_PASSWORD";
